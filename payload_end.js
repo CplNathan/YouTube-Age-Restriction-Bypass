123004487c2fb33d0279193bf658b5ed35925f9b
Nathan <16903800+CplNathan@users.noreply.github.com>
2021-02-17 04:48:29 +0000

    Fixed race condition.
    Fixed an issue where the hook would run before the video player was ready, leading to an incorrect result from getPlayerState()

--- payload_end.js
+++ payload_end.js
@@ -1,5 +1,5 @@
 function removeNode(n) {
-    if (n != null) n.parentNode.removeChild(n);
+    if (n != null) n.parentNode.removeChild(n)
 }
 
 function setElementHide(n, state) {
@@ -16,73 +16,76 @@ function setElementHide(n, state) {
 function waitForNodeId(node, cb) {
     var ival = setInterval(function (node, cb) {
         if (document.getElementById(node) != null) {
-            clearInterval(ival);
-            cb();
+            clearInterval(ival)
+            cb()
         }
-    }, 100, node, cb);
+    }, 100, node, cb)
 }
 
 window.addEventListener('yt-navigate-start', function () {
-    runBypass();
-});
+    runBypass()
+})
 
 window.addEventListener('popstate', function () {
-    runBypass();
-});
+    runBypass()
+})
 
 var overriddenVideo = null
 
 function runBypass() {
     if (overriddenVideo != null) {
-        console.log("Navigating away from page, removing iframe");
+        console.log("Navigating away from page, removing iframe")
 
-        setElementHide(document.getElementById('error-screen'), false);
-        setElementHide(document.getElementsByClassName('ytp-error')[0], false);
-        setElementHide(document.getElementById("movie_player"), false);
+        setElementHide(document.getElementById('error-screen'), false)
+        setElementHide(document.getElementsByClassName('ytp-error')[0], false)
+        setElementHide(document.getElementById("movie_player"), false)
 
-        overriddenVideo.remove();
+        overriddenVideo.remove()
     }
 
-    waitForNodeId('movie_player', function () {
-        let player = document.getElementById("movie_player");
-        let watch = (location.pathname.startsWith('/watch') || location.pathname.startsWith('/embed')) && player.getPlayerState() == -1
-        if (watch) {
+    let playerpage = (location.pathname.startsWith('/watch') || location.pathname.startsWith('/embed'))
+    if (playerpage) {
+        waitForNodeId('movie_player', function () {
             waitForNodeId('player-container', function () {
-                setElementHide(document.getElementById('error-screen'), true);
-                setElementHide(document.getElementsByClassName('ytp-error')[0], true);
-                setElementHide(document.getElementById("movie_player"), true);
+                let player = document.getElementById("movie_player")
+                let watch = playerpage && (player.getPlayerState() == -1 || player.getPlayerState() == 3)
+                if (watch) {
+                    setElementHide(document.getElementById('error-screen'), true)
+                    setElementHide(document.getElementsByClassName('ytp-error')[0], true)
+                    setElementHide(document.getElementById("movie_player"), true)
 
-                waitForNodeId('container', function () {
-                    fetch('https://example.com/?id=' + player.getVideoData().video_id, {
-                        mode: 'cors'
-                    }).then(result => result.json()).then(result => {
-                        var container = document.querySelector('div.ytd-player');
+                    waitForNodeId('container', function () {
+                        fetch('https://example.com/?id=' + player.getVideoData().video_id, {
+                            mode: 'cors'
+                        }).then(result => result.json()).then(result => {
+                            var container = document.querySelector('div.ytd-player')
 
-                        var videoplayer = document.createElement('div');
-                        videoplayer.className = "html5-video-player ytp-transparent ytp-hide-info-bar ytp-large-width-mode iv-module-loaded paused-mode";
+                            var videoplayer = document.createElement('div')
+                            videoplayer.className = "html5-video-player ytp-transparent ytp-hide-info-bar ytp-large-width-mode iv-module-loaded paused-mode"
 
-                        var playercontent = document.createElement('div');
-                        playercontent.className = "ytp-player-content ytp-iv-player-content";
+                            var playercontent = document.createElement('div')
+                            playercontent.className = "ytp-player-content ytp-iv-player-content"
 
-                        var playerframe = window.document.createElement("iframe");
-                        playerframe.setAttribute("src", result.url);
-                        playerframe.setAttribute("id", "movie_player_fake");
-                        playerframe.setAttribute("frameBorder", "0");
-                        playerframe.setAttribute("width", "100%");
-                        playerframe.setAttribute("height", "100%");
-                        playerframe.setAttribute("allowfullscreen", true)
+                            var playerframe = window.document.createElement("iframe")
+                            playerframe.setAttribute("src", result.url)
+                            playerframe.setAttribute("id", "movie_player_fake")
+                            playerframe.setAttribute("frameBorder", "0")
+                            playerframe.setAttribute("width", "100%")
+                            playerframe.setAttribute("height", "100%")
+                            playerframe.setAttribute("allowfullscreen", true)
 
-                        container.appendChild(videoplayer);
-                        videoplayer.appendChild(playercontent);
-                        playercontent.appendChild(playerframe);
+                            container.appendChild(videoplayer)
+                            videoplayer.appendChild(playercontent)
+                            playercontent.appendChild(playerframe)
 
-                        overriddenVideo = videoplayer;
-                        console.log("injected new video");
+                            overriddenVideo = videoplayer
+                            console.log("injected new video")
+                        })
                     })
-                });
-            });
-        }
-    })
+                }
+            })
+        })
+    }
 }
 
-runBypass();
+runBypass()
