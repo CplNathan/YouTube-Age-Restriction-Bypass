cef5e5b0a1dd3441289f63c2108b0045a1a72e3d
Nathan <16903800+CplNathan@users.noreply.github.com>
2021-02-20 22:18:18 +0000

    Directory Cleanup

--- injector_start.js
+++ injector_start.js
@@ -2,7 +2,7 @@ chrome.storage.sync.get({
     downloadServer: ''
 }, function (items) {
     var payloadScript = document.createElement('script');
-    payloadScript.setAttribute('src', chrome.extension.getURL('payload_end.js'));
+    payloadScript.setAttribute('src', chrome.extension.getURL('payload_start.js'));
     payloadScript.setAttribute('data-url', items.downloadServer)
     payloadScript.onload = function () {
         this.remove();
